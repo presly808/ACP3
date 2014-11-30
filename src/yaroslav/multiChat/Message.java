bde3f350e6491cc262760850355b8d5a2cba8066
Администратор <byaroslav@bk.ru>
2014-11-30 22:03:13 +0200

    Первая вверсия мультичата. Почему то не приходят сообщения клиентам хотя они вроде отправдяються.

---
+++ src/yaroslav/multiChat/Message.java
@@ -0,0 +1,52 @@
+package yaroslav.multiChat;
+
+import java.util.Date;
+
+/**
+ * Created by Администратор on 30.11.2014.
+ */
+public class Message {
+    private String msg;
+    private Date date;
+    private String from;
+
+    public Message() {
+    }
+
+    public Message(String msg, Date date, String from) {
+        this.msg = msg;
+        this.date = date;
+        this.from = from;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return date + "   " + from + " пишет: " + msg;
+    }
+}
