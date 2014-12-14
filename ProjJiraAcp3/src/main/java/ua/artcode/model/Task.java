38c41426909b06cad8ee97db1a72ae4583a1ab24
sergii cha <mountin2@gmail.com>
2014-12-14 18:22:21 +0200

    - add Tasks2

--- ProjJiraAcp3/src/main/java/ua/artcode/model/Task.java
+++ ProjJiraAcp3/src/main/java/ua/artcode/model/Task.java
@@ -1,5 +1,7 @@
 package ua.artcode.model;
 
+import java.util.Date;
+
 /**
  * Created by admin on 07.12.2014.
  */
@@ -7,13 +9,18 @@ public class Task {
 
     enum Status { NEW, IN_PROGRESS, REOPENED, CLOSED, DONE};
 
+    protected Date dateStart;
+    protected Date dateFinish;
+    protected int hoursEstimate;
+    protected int hoursSpent;
+    protected String author;
+
     public int createNewTask(){
         return 0;
     }
     public boolean deleteTask(int id){
         return true;
     }
-
 
 
 }
