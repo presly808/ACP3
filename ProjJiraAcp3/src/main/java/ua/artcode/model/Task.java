a3ce7086e790497aa6b86cc2073f4736f0a25cb2
sergii cha <mountin2@gmail.com>
2014-12-14 18:18:04 +0200

    - add Tasks

--- ProjJiraAcp3/src/main/java/ua/artcode/model/Task.java
+++ ProjJiraAcp3/src/main/java/ua/artcode/model/Task.java
@@ -4,4 +4,16 @@ package ua.artcode.model;
  * Created by admin on 07.12.2014.
  */
 public class Task {
+
+    enum Status { NEW, IN_PROGRESS, REOPENED, CLOSED, DONE};
+
+    public int createNewTask(){
+        return 0;
+    }
+    public boolean deleteTask(int id){
+        return true;
+    }
+
+
+
 }
